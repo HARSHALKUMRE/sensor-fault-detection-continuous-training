b46e7c69d6fef959f98468e72da3337d71744d6e
Harshal Kumre <kumreharshalkumar@gmail.com>
2023-07-08 00:10:50 +0530

    code updated

--- train.py
+++ train.py
@@ -1,12 +1,10 @@
 from src.pipeline.training_pipeline import start_training_pipeline
-from src.pipeline.batch_prediction import start_batch_prediction
+
 
 file_path=r"G:\100-days-of-dl\Krish_Naik\FSDS_Ineuron_Course\projects\sensor-fault-detection-continuous-training\notebooks\aps_failure_training_set1.csv"
 print(__name__)
 if __name__=="__main__":
-     try:
-          #start_training_pipeline()
-          output_file = start_batch_prediction(input_file_path=file_path)
-          print(output_file)
-     except Exception as e:
-          print(e)
+    try:
+        start_training_pipeline()
+    except Exception as e:
+        print(e)
